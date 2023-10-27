49e445d6505d785361a9222572eaadd04060acc2
ashraful-commits <ashrfulalam10000@gmail.com>
2023-10-28 00:14:24 +0600

    upddeted

--- Client/src/Pages/Chat.jsx
+++ Client/src/Pages/Chat.jsx
@@ -21,8 +21,8 @@ const Chat = () => {
   const dispatch = useDispatch();
   const [currentUser, setCurrentUser] = useState({});
   const [currentChat, setCurrentChat] = useState(undefined);
-  const ENDPOINT = "http://localhost:3030";
-  // const ENDPOINT = "https://chatapps-v5tm.onrender.com";
+  // const ENDPOINT = "http://localhost:3030";
+  const ENDPOINT = "https://chatapps-v5tm.onrender.com";
   const userData = JSON.parse(localStorage.getItem("LoggedInUser"));
   const [socket, setSocket] = useState(null);
   const [onlineUser, setOnlineUser] = useState(null);
